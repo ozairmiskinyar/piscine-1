77cd9d55ee0f15b8172ad69cf2e46283d8311aef
root <root@DESKTOP-43QCTFI.>
2025-05-05 10:49:50 +0200

    mise en jour

---
+++ src/main.c
@@ -0,0 +1,12 @@
+ /* 
+      * main.c
+      * Un simple programme qui affiche Hello, World!
+      */
+     
+     #include <stdio.h>
+     
+     int main(void) {
+         printf(Hello, New Project!n);
+         return 0;
+     }
+     
